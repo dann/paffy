e472fb2bf8d99d36808094acc46b4f4d7f11aa57
Dann <techmemo@gmail.com>
2008-05-12 21:29:54 +0900

    fixed handles

--- lib/Paffy/Cache.pm
+++ lib/Paffy/Cache.pm
@@ -3,8 +3,14 @@ use CHI;
 use Moose;
 
 has 'cache' => (
-    is => 'rw',
-    handles => [ qw(fetch store remove clear get_keys get_namespaces) ],
+    is      => 'rw',
+    handles => [
+        qw(
+            get set compute remove expire expire_if get_expires_at
+            get_object clear get_keys is_empty purge get_namespaces
+            get_multi_arrayref get_multi_hashref set_multi remove_multi dump_as_hash
+        )
+    ],
 );
 
 no Moose;
