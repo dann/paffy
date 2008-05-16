819506d408f281ac266b40eab4471220dbc66f33
Dann <techmemo@gmail.com>
2008-05-17 03:44:14 +0900

    use CHI as base class.

--- lib/Paffy/Cache.pm
+++ lib/Paffy/Cache.pm
@@ -1,18 +1,8 @@
 package Paffy::Cache;
 use Moose;
-
-has 'cache' => (
-    is      => 'rw',
-    handles => [
-        qw(
-            get set compute remove expire expire_if get_expires_at
-            get_object clear get_keys is_empty purge get_namespaces
-            get_multi_arrayref get_multi_hashref set_multi remove_multi dump_as_hash
-        )
-    ],
-);
-
-no Moose;
+BEGIN {
+    extends qw(CHI Paffy::Class);
+}
 
 __PACKAGE__->meta->make_immutable;
 
