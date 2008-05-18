66ab9a609d733647f5365519f23351bd05504372
Dann <techmemo@gmail.com>
2008-05-19 00:28:18 +0900

    cache resultset when find method is used

--- lib/Paffy/ORM/DBIx/Class/ResultSet.pm
+++ lib/Paffy/ORM/DBIx/Class/ResultSet.pm
@@ -1,10 +1,10 @@
 package Paffy::ORM::DBIx::Class::ResultSet;
 use base qw(DBIx::Class::ResultSet);
 
-sub find_cached {
+sub find {
     my ( $self, $cond, $attrs ) = @_;
     $attrs = {} unless $attrs;
-    return $self->find( $cond, { %$attrs, cached => [qw(id)] } );
+    return $self->SUPER::find( $cond, { %$attrs, cached => [qw(id)] } );
 }
 
 1;
