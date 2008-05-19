f67a590c4016a58d79d2d0716e51353825e29de0
Dann <techmemo@gmail.com>
2008-05-19 23:45:47 +0900

    perltidy

--- lib/Paffy/ORM/DBIx/Class/Storage/DBI/Cached.pm
+++ lib/Paffy/ORM/DBIx/Class/Storage/DBI/Cached.pm
@@ -9,66 +9,72 @@ __PACKAGE__->mk_accessors(qw/cache/);
 
 sub select_single {
     my $self = shift;
-    my ($ident, $select, $condition, $attrs) = @_;
-    if ($attrs->{cached}) {
-        my $cache_key = $self->_check_cacheable($ident, $condition, $attrs->{cached});
-        if($cache_key){
-            if(my $cached_obj = $self->cache->get($cache_key)){
-                if(ref $cached_obj eq "ARRAY"){
-                    warn "[hit cache key] ". $cache_key;
+    my ( $ident, $select, $condition, $attrs ) = @_;
+    if ( $attrs->{cached} ) {
+        my $cache_key
+            = $self->_check_cacheable( $ident, $condition, $attrs->{cached} );
+        if ($cache_key) {
+            if ( my $cached_obj = $self->cache->get($cache_key) ) {
+                if ( ref $cached_obj eq "ARRAY" ) {
+                    warn "[hit cache key] " . $cache_key;
                     return @$cached_obj;
                 }
             }
-            my ($rv, $sth, @bind) = $self->_select($ident, $select, $condition, $attrs);
+            my ( $rv, $sth, @bind )
+                = $self->_select( $ident, $select, $condition, $attrs );
             my @row = $sth->fetchrow_array;
             $sth->finish();
-            $self->cache->set($cache_key, \@row, $CACHE_EXPIRE);
-            warn "[set cache key] ". $cache_key;
+            $self->cache->set( $cache_key, \@row, $CACHE_EXPIRE );
+            warn "[set cache key] " . $cache_key;
             return @row;
         }
     }
-    return $self->SUPER::select_single($ident, $select, $condition, $attrs);
+    return $self->SUPER::select_single( $ident, $select, $condition, $attrs );
 }
 
 sub _check_cacheable {
-    my ($self, $ident, $condition, $cache_key_columns) = @_;
+    my ( $self, $ident, $condition, $cache_key_columns ) = @_;
     my $table;
-    return unless (
-        ref $ident eq 'ARRAY' && scalar @$ident == 1 &&
-            ref $ident->[0] eq 'HASH' && ($table = $ident->[0]->{me})
-        );
+    return
+        unless ( ref $ident eq 'ARRAY'
+        && scalar @$ident == 1
+        && ref $ident->[0] eq 'HASH'
+        && ( $table = $ident->[0]->{me} ) );
     my @key_columns = @$cache_key_columns;
     return unless scalar @key_columns;
     my $cache_key;
-    if(ref $condition eq 'HASH' && $condition->{'-and'} && ref $condition->{'-and'} eq 'ARRAY'){
+    if (   ref $condition eq 'HASH'
+        && $condition->{'-and'}
+        && ref $condition->{'-and'} eq 'ARRAY' )
+    {
         for my $key_col (@key_columns) {
-            if (ref $key_col eq 'ARRAY') {
+            if ( ref $key_col eq 'ARRAY' ) {
                 my %key_set_hash = map { "me." . $_ => $_ } @$key_col;
                 my %res;
-                for my $cond (@{$condition->{'-and'}}){
+                for my $cond ( @{ $condition->{'-and'} } ) {
                     next unless ref $cond eq 'HASH';
-                    for my $key (keys %$cond){
+                    for my $key ( keys %$cond ) {
                         my $k = delete $key_set_hash{$key};
                         $res{$k} = $cond->{$key} if defined $k;
                     }
                 }
-                unless (scalar keys %key_set_hash){
-                    return sprintf(
-                        "%s::%s::%s::%s",
+                unless ( scalar keys %key_set_hash ) {
+                    return sprintf( "%s::%s::%s::%s",
                         $self->{__cache_prefix} || ref $self,
-                        join("", map { ucfirst($_)} split("_", $table)),
-                        join("+", keys %res),
-                        join("+", values %res)
-                    );
+                        join( "", map { ucfirst($_) } split( "_", $table ) ),
+                        join( "+", keys %res ),
+                        join( "+", values %res ) );
                 }
-            } else {
-                for my $cond (@{$condition->{'-and'}}){
+            }
+            else {
+                for my $cond ( @{ $condition->{'-and'} } ) {
                     next unless ref $cond eq 'HASH';
-                    for my $key (keys %$cond){
+                    for my $key ( keys %$cond ) {
                         return sprintf(
                             "%s::%s::%s::%s",
                             $self->{__cache_prefix} || ref $self,
-                            join("", map { ucfirst($_)} split("_", $table)),
+                            join( "",
+                                map { ucfirst($_) } split( "_", $table ) ),
                             $key_col,
                             $cond->{$key}
                         ) if $key eq "me.$key_col";
@@ -76,16 +82,19 @@ sub _check_cacheable {
                 }
             }
         }
-    } elsif ( ref $condition eq 'ARRAY' && scalar @$condition == 1 && ref $condition->[0] eq 'HASH' ) {
+    }
+    elsif (ref $condition eq 'ARRAY'
+        && scalar @$condition == 1
+        && ref $condition->[0] eq 'HASH' )
+    {
         for my $key_col (@key_columns) {
             next if ref $key_col;
-            return sprintf(
-                "%s::%s::%s::%s",
+            return sprintf( "%s::%s::%s::%s",
                 $self->{__cache_prefix} || ref $self,
-                join("", map { ucfirst($_)} split("_", $table)),
+                join( "", map { ucfirst($_) } split( "_", $table ) ),
                 $key_col,
-                $condition->[0]->{"me.$key_col"}
-            ) if defined $condition->[0]->{"me.$key_col"};
+                $condition->[0]->{"me.$key_col"} )
+                if defined $condition->[0]->{"me.$key_col"};
         }
     }
     return;
